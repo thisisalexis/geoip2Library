1b123dcd4d365af7c5e5ac84b515695119537eb8
Alexis José Bravo Llovera <alexis.ve@gmail.com>
2018-02-21 11:57:31 -0500

    Http module added to the imports in the Geoip2module

--- src/app/modules/geoip2/geoip2.module.ts
+++ src/app/modules/geoip2/geoip2.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {Geoip2Service} from './services/geoip2/geoip2.service';
+import {HttpModule} from '@angular/http';
 import {LocalStorageModule} from 'angular-2-local-storage';
+import {Geoip2Service} from './services/geoip2/geoip2.service';
 
 @NgModule({
   imports: [
     CommonModule,
+    HttpModule,
     LocalStorageModule.withConfig({
       prefix: 'geoip2',
       storageType: 'localStorage'
